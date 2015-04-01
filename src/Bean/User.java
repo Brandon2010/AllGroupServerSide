bbbebdf5f14de8fbf7c60192ea2fa64cfec91972
Nirosun <zuozhengyang@gmail.com>
2015-04-01 15:49:48 -0400

    fix User...

--- src/Bean/User.java
+++ src/Bean/User.java
@@ -1,5 +1,42 @@
 package Bean;
 
-public class User {
+import java.math.BigInteger;
 
+public class User {
+	private long userId;
+	private BigInteger facebookId;
+	private String name;
+	
+	public User(long userId, BigInteger facebookId, String name) {
+		super();
+		this.userId = userId;
+		this.facebookId = facebookId;
+		this.name = name;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public BigInteger getFacebookId() {
+		return facebookId;
+	}
+
+	public void setFacebookId(BigInteger facebookId) {
+		this.facebookId = facebookId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
