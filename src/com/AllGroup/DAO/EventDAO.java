049f948487fb70fa680f2d718295d83a137bdd8c
Brandon2010 <wxbrandon2010@gmail.com>
2015-04-02 15:44:31 -0400

    Finish the EventDAO

--- src/com/AllGroup/DAO/EventDAO.java
+++ src/com/AllGroup/DAO/EventDAO.java
@@ -12,7 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.AllGroup.Util.DataAccess;
-import com.AllGroup.Bean.Event;;
+import com.AllGroup.Bean.Event;
+import com.AllGroup.Bean.Participant;
 
 /**
  * @author wangxi
@@ -56,7 +57,7 @@ public class EventDAO {
 		return updateRows;
 	}
 	
-	public List<Event> findProducts(String searchKey, String searchValue) {
+	public List<Event> findEvents(String searchKey, String searchValue) {
 		Connection dbCon = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
@@ -68,7 +69,14 @@ public class EventDAO {
 			ps = dbCon.prepareStatement(sql);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				//Event event = new Event(rs.getLong());
+				Event event = new Event();
+				event.setEventId(rs.getLong("event_id"));
+				event.setCateId(rs.getLong("cate_id"));
+				event.setName(rs.getString("name"));
+				event.setTime(rs.getString("time"));
+				event.setLocation(rs.getString("location"));
+				event.setDescription(rs.getString("description"));
+				results.add(event);
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -81,5 +89,169 @@ public class EventDAO {
 			}
 		}
 		return results;
+	}
+	
+	public Event getEventById(long eventId) {
+		Connection dbCon = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = null;
+		try {
+			dbCon = DataAccess.getConnection();
+			sql = "SELECT * FROM event e where e.event_id = ?";
+			ps = dbCon.prepareStatement(sql);
+			ps.setLong(1, eventId);
+			rs = ps.executeQuery();
+			if (rs.next()) {
+				Event event = new Event();
+				event.setEventId(rs.getLong("event_id"));
+				event.setCateId(rs.getLong("cate_id"));
+				event.setName(rs.getString("name"));
+				event.setTime(rs.getString("time"));
+				event.setLocation(rs.getString("location"));
+				event.setDescription(rs.getString("description"));
+				return event;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				DataAccess.close(rs, ps, dbCon);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	public List<Event> getEventsByCate(long cateId) {
+		Connection dbCon = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = null;
+		List<Event> results = new ArrayList<Event>();
+		try {
+			dbCon = DataAccess.getConnection();
+			sql = "SELECT * FROM event e where e.cate_id = ?";
+			ps = dbCon.prepareStatement(sql);
+			ps.setLong(1, cateId);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Event event = new Event();
+				event.setEventId(rs.getLong("event_id"));
+				event.setCateId(rs.getLong("cate_id"));
+				event.setName(rs.getString("name"));
+				event.setTime(rs.getString("time"));
+				event.setLocation(rs.getString("location"));
+				event.setDescription(rs.getString("description"));
+				results.add(event);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				DataAccess.close(rs, ps, dbCon);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return results;
+	}
+	
+	public int deleteEvent(long eventId) {
+		Connection dbCon = null;
+		PreparedStatement ps = null;
+		int updateRows = 0;
+		String sql = null;
+		try {
+			dbCon = DataAccess.getConnection();
+			dbCon.setAutoCommit(false);
+			sql = "delete from event where eventId = ?";
+			ps = dbCon.prepareStatement(sql);
+			ps.setLong(1, eventId);
+			updateRows = ps.executeUpdate();
+			dbCon.commit();
+		} catch (Exception e) {
+			try {
+				dbCon.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			try {
+				DataAccess.close(ps, dbCon);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return updateRows;
+	}
+	
+	public List<Participant> getParticipantsByEvent(long eventId) {
+		Connection dbCon = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = null;
+		List<Participant> results = new ArrayList<Participant>();
+		try {
+			dbCon = DataAccess.getConnection();
+			sql = "SELECT * FROM particpant p where p.event_id = ?";
+			ps = dbCon.prepareStatement(sql);
+			ps.setLong(1, eventId);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				Participant p = new Participant();
+				p.setEventId(eventId);
+				p.setPartId(rs.getLong("part_id"));
+				p.setUserId(rs.getLong("user_id"));
+				results.add(p);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			try {
+				DataAccess.close(rs, ps, dbCon);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return results;
+	}
+	
+	public int addParticipant(long user_id, long event_id) {
+		Connection dbCon = null;
+		PreparedStatement ps = null;
+		int updateRows = 0;
+		String sql = null;
+		try {
+			dbCon = DataAccess.getConnection();
+			dbCon.setAutoCommit(false);
+			sql = "insert into participant (user_id, event_id) values (?, ?)";
+			ps = dbCon.prepareStatement(sql);
+			ps.setLong(1, user_id);
+			ps.setLong(2, event_id);
+			updateRows = ps.executeUpdate();
+			dbCon.commit();
+		} catch (Exception e) {
+			try {
+				dbCon.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		} finally {
+			try {
+				DataAccess.close(ps, dbCon);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return updateRows;
 	}
 }
